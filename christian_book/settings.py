c4af941d630a6c3d7fc4238e3656cee7f1dd3c89
Jason Bongiovanni <jpbon@protonmail.com>
2022-06-07 16:47:27 -0400

    deployment

--- christian_book/settings.py
+++ christian_book/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'christian_book.urls'
@@ -121,9 +122,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@ Issues:
 1. url string vs integer
 2. ampersands not rendering properly (used auto escape off)
 3. json upload and added to database without duplicating
+
+Possible Issues:
+There is no validation to check if the POST data in the upload field is json data. Since this application would not be made public
+and only used by the company, no validation would be necessary to protect the database from a possible security breach. Unless
+it was deemed necessary just in case there is some sort of disgruntled employee.
